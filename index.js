510bd89baa0fd023281ce81a7b358157d10f6252
z46-dev <45317380+z46-dev@users.noreply.github.com>
2024-03-12 21:28:06 -0400

    Add WebSocket functionality for creating and training neural network

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// TODO:
-// 1. Create neural network and then train and save it.
-// 2. Allow a user to upload a test and then test the neural network after loading it.
-
+import { WebSocketServer } from "ws";
 import ImagesBuffer from "./lib/ImagesBuffer.js";
 import Server from "./lib/Server.js";
 import NeuralNetwork from "./lib/NeuralNetwork.js";
 import { loadTest, saveTest } from "./lib/saveTest.js";
 
 const webServer = new Server(80);
+/**
+ * @type {WebSocketServer}
+ */
+const webSocketServer = webServer.createWebSocketServer(WebSocketServer);
+
 webServer.publicize("./public");
 
 webServer.post("/createTest", function processRequest(request, response) {
@@ -48,6 +50,79 @@ webServer.post("/test", function processRequest(request, response) {
     const output = network.predict(Array.from(new Uint8Array(body)));
 
     response.send(output.indexOf(Math.max(...output)).toString());
+});
+
+webSocketServer.on("connection", function connection(socket) {
+    socket.binaryType = "arraybuffer";
+
+    function talk(type, data) {
+        const buffer = Buffer.alloc(11);
+        switch (type) {
+            case "progress":
+                buffer.writeUint8(0, 0);
+                buffer.writeUint8(data * 256 | 0, 1);
+                break;
+            case "beginning":
+                buffer.writeUint8(1, 0);
+                buffer.writeUint16LE(data.imageCount, 1);
+                buffer.writeUint16LE(data.imageSize, 3);
+                buffer.writeUint8(data.labelsSize, 5);
+                buffer.writeUint32LE(data.packetSize, 6);
+                break;
+            case "progress":
+                buffer.writeUint8(2, 0);
+                buffer.writeFloatLE(data, 1);
+                break;
+            case "end":
+                buffer.writeUint8(3, 0);
+                break;
+        }
+
+        socket.send(buffer);
+    }
+
+    /**
+     * @param {Buffer} buffer 
+     */
+    function begin(buffer) {
+        const imagesBuffer = ImagesBuffer.fromBuffer(buffer);
+        const network = new NeuralNetwork(imagesBuffer.size ** 2, 1024, 128, imagesBuffer.labelsSize);
+
+        talk("beginning", {
+            imageCount: imagesBuffer.images.length,
+            imageSize: imagesBuffer.size,
+            labelsSize: imagesBuffer.labelsSize,
+            packetSize: buffer.length
+        });
+
+        for (let i = 0; i < imagesBuffer.images.length; i++) {
+            const output = new Array(imagesBuffer.labelsSize).fill(0);
+            output[imagesBuffer.labels[i]] = 1;
+
+            network.train(imagesBuffer.images[i], output);
+
+            if (i % 50 === 0) {
+                talk("progress", i / imagesBuffer.images.length);
+            }
+        }
+
+        talk("end");
+
+        // Save
+        saveTest("t4", imagesBuffer.toBuffer(), network.serialize());
+    }
+
+    const buffers = [];
+    socket.onmessage = function onmessage(event) {
+        const buffer = Buffer.from(event.data);
+
+        if (buffer.at(0) === 0) {
+            begin(Buffer.concat(buffers));
+            buffers.length = 0;
+        } else {
+            buffers.push(buffer.slice(1));
+        }
+    };
 });
 
 webServer.listen();
--- public/index.js
+++ public/index.js
@@ -81,3 +81,52 @@ async function multipleTests(n = 10) {
 window.createAndTrain = createAndTrain;
 window.testNetwork = testNetwork;
 window.multipleTests = multipleTests;
+
+function createAndTrainWS() {
+    const socket = new WebSocket("ws://localhost:80");
+    socket.binaryType = "arraybuffer";
+
+    socket.onopen = function() {
+        console.log("Connected to server.")
+        const images = [];
+
+        for (let i = 0; i < values.length; i++) {
+            for (let j = 0; j < 500; j++) {
+                images.push({
+                    image: generateNumberImage(i, imageSize),
+                    label: i
+                });
+            }
+        }
+
+        const uint8Array = new Uint8Array(images.length * imageSize * imageSize + 4 + images.length);
+        uint8Array.set([images.length >> 8, images.length & 0xFF, imageSize, values.length], 0);
+
+        for (let i = 0; i < images.length; i++) {
+            uint8Array.set([images[i].label], 4 + i);
+            uint8Array.set(images[i].image, 4 + images.length + i * imageSize * imageSize);
+        }
+
+        let i = 0,
+            packetSize = 1024 * 4;
+
+        console.log("Sending");
+
+        while (i < uint8Array.length) {
+            const packet = new Uint8Array(1 + Math.min(packetSize, uint8Array.length - i));
+            packet.set([1], 0);
+            packet.set(uint8Array.slice(i, i + packetSize), 1);
+            i += packetSize;
+
+            socket.send(packet);
+        }
+
+        socket.send(new Uint8Array([0]));
+    };
+
+    socket.onmessage = function(event) {
+        console.log(`Neural Network Info: ${new Uint8Array(event.data)}`);
+    };
+}
+
+window.createAndTrainWS = createAndTrainWS;
